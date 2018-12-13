6ea6c64e4a4e999fdb6e240f659cb1e879b4c761
ravshanmakhmadaliev <ravshan9779@mail.ru>
2018-12-13 15:07:16 +0500

    test Toast added

--- app/src/main/java/app/repbulic/order/orderrepublic/MainActivity.java
+++ app/src/main/java/app/repbulic/order/orderrepublic/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -86,6 +87,7 @@ public class MainActivity extends AppCompatActivity
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+        Toast.makeText(this, "Hello world", Toast.LENGTH_SHORT).show();
         return true;
     }
 }
