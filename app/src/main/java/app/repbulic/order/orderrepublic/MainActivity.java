1a8c8831e9666ccf45cbc838815293d6648063ce
RakhimjonRustamov <r.rustamov@gmail.com>
2018-12-13 22:37:43 +0500

    Profile Fragment

--- app/src/main/java/app/repbulic/order/orderrepublic/MainActivity.java
+++ app/src/main/java/app/repbulic/order/orderrepublic/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,9 +15,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import app.repbulic.order.orderrepublic.iu.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -74,18 +78,21 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        Fragment fragment=null;
         int id = item.getItemId();
 
         if (id == R.id.nav_favorites) {
             // Handle the camera action
         } else if (id == R.id.nav_profile) {
-
+            fragment = ProfileFragment.newInstance();
         } else if (id == R.id.nav_orders) {
 
         } else if (id == R.id.nav_about) {
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.place_holder, fragment) .commit();
         return true;
     }
 }
