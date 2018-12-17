1b8a8a93034701bc2983f5dec55b82a56299815a
ravshanmakhmadaliev <ravshan9779@mail.ru>
2018-12-17 21:49:06 +0500

    small change to check

--- app/src/main/java/app/repbulic/order/orderrepublic/MainActivity.java
+++ app/src/main/java/app/repbulic/order/orderrepublic/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
+        //check
     }
 
     @Override
