eefe92d8001628833f7a3d4bb42d63039f9b140e
RakhimjonRustamov <r.rustamov@gmail.com>
2018-12-22 10:57:46 +0500

    CartSingleRestaurantOrder need to be finished

--- app/src/main/java/app/repbulic/order/orderrepublic/adapters/CartSingleRestaurantAdapter.java
+++ app/src/main/java/app/repbulic/order/orderrepublic/adapters/CartSingleRestaurantAdapter.java
@@ -10,9 +10,11 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 import app.repbulic.order.orderrepublic.R;
+import app.repbulic.order.orderrepublic.models.Food;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -20,14 +22,15 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class CartSingleRestaurantAdapter extends RecyclerView.Adapter<CartSingleRestaurantAdapter.ViewHolder> {
 
   private Context rcontext;
-  private int quantity=12;
-  private List<String> cartList;
+  private double foodPrice;
+  private List<Food> cartList;
   private static OnItemClickListener onItemClickListener;
 
-  public CartSingleRestaurantAdapter(Context context, List<String> list) {
+  public CartSingleRestaurantAdapter(Context context, List<Food> list) {
     this.cartList = list;
     this.rcontext = context;
   }
+
   public static void setOnItemClickListener(OnItemClickListener onItemClickListener) {
     CartSingleRestaurantAdapter.onItemClickListener = onItemClickListener;
   }
@@ -41,15 +44,51 @@ public class CartSingleRestaurantAdapter extends RecyclerView.Adapter<CartSingle
 
 
   @Override
-  public void onBindViewHolder(ViewHolder holder, int position) {
+  public void onBindViewHolder(final ViewHolder holder, int position) {
+    final Food food = cartList.get(position);
     Glide.with(rcontext)
       .asBitmap()
-      .load(cartList.get(position))
+      .load(food.getPictureLink())
       .into(holder.singleImage);
+    final int price = Integer.parseInt(food.getPrice());
+    holder.singlePrice.setText(String.valueOf(food.getPrice()));
+    holder.singleTopPrice.setText(String.valueOf(price));
+    holder.singleName.setText(food.getFoodName());
+    holder.elegantNumberButton.setNumber("1");
+    holder.elegantNumberButton.setOnValueChangeListener(new ElegantNumberButton.OnValueChangeListener() {
+      @Override
+      public void onValueChange(ElegantNumberButton view, int oldValue, int newValue) {
+        DecimalFormat newDf = new DecimalFormat("#,###");
+        String newFormat;
+        if (price < 1000) {
+          newDf = new DecimalFormat("###");
+        }
+        if (price > 1000 && price < 10000) {
+          newDf = new DecimalFormat("#,###");
+        }
+        if (price >= 10000) {
+          newDf = new DecimalFormat("##,###");
+        }
+        newFormat = newDf.format(price * newValue);
+        cartList.get(holder.getAdapterPosition()).setPrice(Integer.toString(price * newValue));
+        holder.singleTopPrice.setText(newFormat);
+        onItemClickListener.calculateTotal(cartList);
+      }
+    });
 
-    // single food price
-    // top price is on top of single cart item  quantity * single price
-    // food name
+    holder.deleteCartOrder.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        deleteFromCart(food.getFoodId(), holder.getAdapterPosition());
+      }
+    });
+  }
+
+  private void deleteFromCart(String foodId, int position){
+    onItemClickListener.itemDeleted(foodId);
+    cartList.remove(position);
+    onItemClickListener.calculateTotal(cartList);
+    notifyItemRemoved(position);
   }
 
   @Override
@@ -57,7 +96,7 @@ public class CartSingleRestaurantAdapter extends RecyclerView.Adapter<CartSingle
     return cartList.size();
   }
 
-  public class ViewHolder extends RecyclerView.ViewHolder implements ElegantNumberButton.OnClickListener {
+  public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     @BindView(R.id.cart_delete_btn)
     TextView deleteCartOrder;
     @BindView(R.id.cart_single_image)
@@ -68,29 +107,25 @@ public class CartSingleRestaurantAdapter extends RecyclerView.Adapter<CartSingle
     TextView singlePrice;
     @BindView(R.id.cart_top_price)
     TextView singleTopPrice;
-    @BindView(R.id.cart_elegant_btn) ElegantNumberButton elegantNumberButton;
+    @BindView(R.id.elegant_btn)
+    ElegantNumberButton elegantNumberButton;
+
+
     public ViewHolder(View itemView) {
       super(itemView);
-      ButterKnife.bind(this,itemView);
-      elegantNumberButton.setOnClickListener(new ElegantNumberButton.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-          quantity =Integer.parseInt(elegantNumberButton.getNumber());
-          onItemClickListener.onItemClick(getPosition(), cartList, quantity);
-        }
-      });
+      ButterKnife.bind(this, itemView);
+      deleteCartOrder.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
     }
   }
 
   public interface OnItemClickListener {
-    void onItemClick(int position, List<String> returnList, int elegantNumber);
+    void calculateTotal(List<Food> returnList);
+    void itemDeleted(String foodId);
   }
-
 
 }
 
--- app/src/main/java/app/repbulic/order/orderrepublic/models/Order.java
+++ app/src/main/java/app/repbulic/order/orderrepublic/models/Order.java
@@ -130,7 +130,6 @@ public class Order {
                 "-LTq1uzUTmvBLkR1H-Cq");
         orders.add(order);
 
-
         return orders;
     }
 
